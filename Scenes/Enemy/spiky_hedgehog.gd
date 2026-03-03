f9190652d38c5ffc8d97b7cae99731f4fb799ae8
Ura7 <Makenki99@hotmail.com>
2026-03-03 19:44:10 +0300

    Enemy Chase and Patrol System

--- Scenes/Enemy/spiky_hedgehog.gd
+++ Scenes/Enemy/spiky_hedgehog.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @export var dummy_target: CharacterBody2D
 @export var SPEED: int=50
 @export var CHASE_SPEED: int = 150
+@export var xSP: int = 300
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var ray_cast_2d: RayCast2D = $AnimatedSprite2D/RayCast2D
@@ -20,5 +21,74 @@ enum States{
 var current_state = States.PATROL
 
 func _ready():
-	left_bounds = self.position + Vector2(-100,0)
-	right_bounds = self.position + Vector2(100,0)
+	left_bounds = self.position + Vector2(-300,0)
+	right_bounds = self.position + Vector2(300,0)
+
+func _physics_process(delta: float) -> void:
+	handle_gravity(delta)
+	enemy_movement(delta)
+	change_direction()
+	collide_player()
+	
+func collide_player():
+	if ray_cast_2d.is_colliding():
+		var collider = ray_cast_2d.get_collider()
+		if collider == dummy_target:
+			chase_player()
+		elif current_state == States.CHASE:
+			stop_chase()
+	elif current_state == States.CHASE:
+		stop_chase()
+		
+func chase_player()->void:
+	timer.stop()
+	current_state = States.CHASE
+
+func stop_chase() -> void:
+	if timer.time_left <=0:
+		timer.start()
+	
+	
+	
+func enemy_movement(delta:float) -> void:
+	if current_state == States.PATROL:
+		velocity = velocity.move_toward(direction * SPEED, xSP* delta)
+	elif current_state == States.CHASE:
+		velocity = velocity.move_toward(direction * CHASE_SPEED,	xSP* delta)
+	move_and_slide()
+	
+	
+func change_direction() -> void:
+	if current_state == States.PATROL:
+		if animated_sprite_2d.flip_h:
+			if self.position.x <= right_bounds.x:
+				direction = Vector2(1,0)
+			else:
+				animated_sprite_2d.flip_h = false
+				ray_cast_2d.target_position = Vector2(-1000,0)
+		else:
+			if self.position.x >= left_bounds.x:
+				direction = Vector2(-1,0)
+			else:
+				animated_sprite_2d.flip_h = true
+				ray_cast_2d.target_position = Vector2(1000,0)
+	elif current_state == States.CHASE:
+		direction = (dummy_target.position - self.position).normalized()
+		direction = sign(direction)
+		if direction.x == 1:
+			animated_sprite_2d.flip_h =true
+			ray_cast_2d.target_position = Vector2(1000,0)
+		else:
+			animated_sprite_2d.flip_h=false
+			ray_cast_2d.target_position=Vector2(-1000,0)
+	
+func handle_gravity(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += gravity*delta
+
+
+
+func _on_timer_timeout():
+	current_state = States.PATROL
+	
+	
--- Scenes/Player/dummy_target.gd
+++ Scenes/Player/dummy_target.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -800.0
 
 func _ready():
 	add_to_group("Player")
