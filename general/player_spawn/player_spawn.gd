dc21ce84637d2d57f3c1d02380aad7790b1dbb5f
Hyper Sonic <1067772+hsandt@users.noreply.github.com>
2026-03-03 14:41:02 +0100

    [PlayerSpawn] Metroidvania tutorial: Player Spawn: finished
    Add player spawn in all test scenes

--- general/player_spawn/player_spawn.gd
+++ general/player_spawn/player_spawn.gd
@@ -1,3 +1,22 @@
 @icon("res://general/icons/player_spawn.svg")
 class_name PlayerSpawn
 extends Node2D
+
+
+@export var player_prefab: PackedScene
+
+
+func _ready() -> void:
+	visible = false
+
+	await get_tree().process_frame
+
+	# Check to see if we already have a player
+	var player := get_tree().get_first_node_in_group(&"Player")
+
+	# If we have a player, do nothing
+	if player:
+		return
+
+	# Instantiate a new instance of player at spawn position
+	NodeUtils.instantiate_under_at(player_prefab, get_tree().root, global_position)
